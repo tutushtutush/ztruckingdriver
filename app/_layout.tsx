db70905a2e27ac39199314285fc3ad2dc27d232f
elezersolomon <elezersolomon2@gmail.com>
2025-03-24 10:06:03 +0300

    refactor(code base structure change): removed "screens" folder from repo
    moved content from "screens" folder to "app" folder and removed "screens" folder

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,9 +4,9 @@ import "./globals.css";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(screens)" options={{ headerShown: false }} />
+      <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(screens)/(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
